0398530907629b9e089fc397a227673b9742d112
Ramprasad Ohnu <rohnu@extrao1.com>
2025-05-25 13:52:19 -0500

    Update create-server-cert.sh

--- certs/create-server-cert.sh
+++ certs/create-server-cert.sh
@@ -1,1 +1,3 @@
+#Step 1: Create a Certificate for Your Local Server -Create a Private Key
+#Create a Certificate Signing Request (CSR)
 openssl genrsa -out server.key 2048 && openssl req -new -key server.key -out server.csr
