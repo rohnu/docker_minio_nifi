848d12761a80317ac87a71fd5de476123c700f10
Ramprasad Ohnu <rohnu@extrao1.com>
2025-05-25 13:54:05 -0500

    Update create-server-cert.sh

--- certs/create-server-cert.sh
+++ certs/create-server-cert.sh
@@ -1,3 +1,7 @@
 #Step 1: Create a Certificate for Your Local Server -Create a Private Key
 #Create a Certificate Signing Request (CSR)
 openssl genrsa -out server.key 2048 && openssl req -new -key server.key -out server.csr
+
+#Sign the Certificate Using Your Root CA
+openssl x509 -req -in server.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out server.crt -days 500 -sha256 -extfile localhost.ext && openssl x509 -in rootCA.pem -text -noout
+
