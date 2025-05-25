311c15e45ed4ec78ea1cf0d672bdf85ae2ed0170
Ramprasad Ohnu <rohnu@extrao1.com>
2025-05-25 13:55:54 -0500

    Update create-server-cert.sh

--- certs/create-server-cert.sh
+++ certs/create-server-cert.sh
@@ -5,3 +5,11 @@ openssl genrsa -out server.key 2048 && openssl req -new -key server.key -out ser
 #Sign the Certificate Using Your Root CA
 openssl x509 -req -in server.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out server.crt -days 500 -sha256 -extfile localhost.ext && openssl x509 -in rootCA.pem -text -noout
 
+openssl pkcs12 -export -in server.crt -inkey server.key -out server.p12
+
+#You should be able to use the resulting file directly using the PKCS12 keystore type or convert it to JKS using keytool -importkeystore
+#JKS Support: NiFi can utilize JKS files for both keystores (containing private keys and certificates) and truststores (containing trusted certificates). 
+
+keytool -importkeystore -srckeystore server.p12  -srcstoretype PKCS12  -destkeystore server.jks  -deststoretype JKS
+keytool -import -file rootCA.pem -alias cacert -keystore truststore.jks -storepass /password/
+
